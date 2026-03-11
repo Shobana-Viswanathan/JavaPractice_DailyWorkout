c5290a6788be7d881f35327c4a5311e9da477eae
Shobana <shobanav112@gmail.com>
2026-03-11 09:54:52 +0530

    demo of jenkins

---
+++ Jenkins_Demo/src/jenkins/OddorEven.java
@@ -0,0 +1,15 @@
+package jenkins;
+import java.util.*;
+public class OddorEven {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number");
+    int num=sc.nextInt();
+    if(num%2==0)
+    	System.out.println("Even");
+    else
+    	System.out.println("Odd");
+	}
+
+}
---
+++ Jenkins_Demo/src/jenkins/PracticeJenkins.java
@@ -0,0 +1,15 @@
+package jenkins;
+import java.util.*;
+public class PracticeJenkins {
+    public static void main(String[] args) {
+    System.out.println("Learning Jenkins");
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Input:");
+    int age=sc.nextInt();
+    sc.nextLine();
+    String name=sc.nextLine();
+    System.out.println("Age:"+age);
+    System.out.println("Name:"+name);
+   
+    }
+}
