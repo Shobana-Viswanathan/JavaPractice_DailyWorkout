49f1eda92f6cc60b27d10260f3de7baca601b4e4
Shobana <shobanav112@gmail.com>
2026-05-10 23:28:09 +0530

    Added add to cart

--- TutorialNinjaCucumber/src/test/java/com/runner/TestNGRunner.java
+++ TutorialNinjaCucumber/src/test/java/com/runner/TestNGRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	    features = "src/test/resources/com/features/login.feature",
+	    features = "src/test/resources/com/features/addtocart.feature",
 	    glue = "com.stepdefinition",
 	    plugin = {
 	        "pretty",
